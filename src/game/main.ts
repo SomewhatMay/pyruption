4c161f41cf75acb3a93816018c5deddcedc93ef4
SomewhatMay <40745122+SomewhatMay@users.noreply.github.com>
2026-03-25 00:44:42 -0400

    initial commit

--- src/game/main.ts
+++ src/game/main.ts
@@ -9,7 +9,7 @@ import { Preloader } from './scenes/Preloader';
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
-    width: 1024,
+    width: 1920,
     height: 768,
     parent: 'game-container',
     backgroundColor: '#028af8',
