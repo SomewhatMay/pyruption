4a4775a15beeb779cf51aa45f16e78c9498a1618
SomewhatMay <40745122+SomewhatMay@users.noreply.github.com>
2026-04-01 00:42:13 -0400

    setFire function

--- src/game/scenes/game/GrassService.ts
+++ src/game/scenes/game/GrassService.ts
@@ -6,12 +6,17 @@ type Grass = {
   x: number;
   y: number;
   state: GrassState;
+  image?: Phaser.GameObjects.Image;
 };
 
 const GRASS_SIZE = 32; // px, length and width (square)
 
+const INITIAL_FIRE_PROBABILITY = 0.1;
+const FIRE_PROBABILITY_INCREASE_RATE = 1.1;
+
 export class GrassService {
   private grassMap: Grass[][];
+  private fireProbability = INITIAL_FIRE_PROBABILITY;
 
   constructor(private boot: Boot) {
     this.grassMap = [];
@@ -29,20 +34,45 @@ export class GrassService {
         };
       }
     }
+
     this.boot.load.image("grass", "assets/grass.png");
+    this.boot.load.image("burning-grass", "assets/burning-grass.png");
+  }
+
+  setFire(grassInfo: Grass) {
+    if (grassInfo.image) {
+      grassInfo.image.destroy();
+    }
+
+    grassInfo.image = this.boot.add
+      .image(
+        grassInfo.x * GRASS_SIZE,
+        grassInfo.y * GRASS_SIZE,
+        "burning-grass"
+      )
+      .setOrigin(0, 0);
+  }
+
+  update() {
+    if (Math.random() > this.fireProbability) {
+      // Start a fire!
+    }
   }
 
   create() {
     // this.boot.add.image(128, 128, "grass");
 
+    // Generate all grass objects
     for (let x = 0; x < this.grassMap.length; x++) {
       for (let y = 0; y < this.grassMap[x].length; y++) {
         const grassInfo = this.grassMap[x][y];
 
         if (grassInfo.state == "alive") {
-          this.boot.add
+          const image = this.boot.add
             .image(x * GRASS_SIZE, y * GRASS_SIZE, "grass")
             .setOrigin(0, 0);
+
+          grassInfo.image = image;
         }
       }
     }
