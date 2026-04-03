1cc884ef4ef527cd8a801f86a858faa6b4f0c58b
SomewhatMay <umayeer.ahsan@hotmail.com>
2026-04-03 01:10:43 -0400

    Update brush size

--- src/game/scenes/game/BrushServiceConstants.ts
+++ src/game/scenes/game/BrushServiceConstants.ts
@@ -1,7 +1,7 @@
 import { GRASS_SIZE } from "./GrassServiceConstants";
 
 export const CURSOR_PX_MULT = GRASS_SIZE;
-export const CURSOR_DEFAULT_SIZE = 4;
+export const CURSOR_DEFAULT_SIZE = 2;
 export const CURSOR_MAX_SIZE = 4;
 export const CURSOR_MIN_SIZE = 1;
 
