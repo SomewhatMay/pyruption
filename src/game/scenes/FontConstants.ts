31acbedaedbad0be057bfe4422635ce3dcc9a3db
SomewhatMay <umayeer.ahsan@hotmail.com>
2026-04-04 00:36:47 -0400

    Arcade default font custom size function

--- src/game/scenes/FontConstants.ts
+++ src/game/scenes/FontConstants.ts
@@ -1,5 +1,15 @@
 export const ARCADE_DEFAULT = {
   fontFamily: "Arcade",
   resolution: 1,
-  fontSize: "10px",
+  fontSize: "12px",
+};
+
+/**
+ *
+ * @param fontSize pixels
+ */
+export const arcadeDefaultResized = (
+  fontSize: number
+): typeof ARCADE_DEFAULT => {
+  return { ...ARCADE_DEFAULT, fontSize: `${fontSize}px` };
 };
