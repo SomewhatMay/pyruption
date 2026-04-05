0d826c883817ed207d5cdc79bbaece454709aaaf
SomewhatMay <umayeer.ahsan@hotmail.com>
2026-04-05 00:18:29 -0400

    update vite config

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -17,7 +17,7 @@ const phasermsg = () => {
 }   
 
 export default defineConfig({
-    base: '/pyruption',
+    base: '/pyruption/',
     logLevel: 'warning',
     build: {
         rollupOptions: {
